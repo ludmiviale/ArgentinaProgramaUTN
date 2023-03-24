ff2b43bf1d6fb16d316ca7f8fe9524e09285ba40
ludmiviale <111137199+ludmiviale@users.noreply.github.com>
2023-03-24 16:54:28 -0300

    Comenzando TP6

---
+++ TP6/src/Paquete1/Calculadora.java
@@ -0,0 +1,28 @@
+package Paquete1;
+
+public class Calculadora {
+
+    public static double Sumar(double unNumero, double otroNumero) {
+        double resultado;
+        resultado = unNumero + otroNumero;
+        return resultado;
+    }
+
+    public static double Restar(double unNumero, double otroNumero) {
+        double resultado;
+        resultado = unNumero - otroNumero;
+        return resultado;
+    }
+
+    public static double Multiplicar(double unNumero, double otroNumero) {
+        double resultado;
+        resultado = unNumero * otroNumero;
+        return resultado;
+    }
+
+    public static double Dividir(double unNumero, double otroNumero) {
+        double resultado;
+        resultado = unNumero / otroNumero;
+        return resultado;
+    }
+}
---
+++ TP6/src/Paquete1/MenuCalculadora.java
@@ -0,0 +1,18 @@
+package Paquete1;
+
+import java.util.Scanner;
+
+public class MenuCalculadora {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        double numero1, numero2, resultado;
+
+        System.out.println("Introduce el primer número:");
+        numero1 = reader.nextDouble();
+
+        System.out.println("Introduce el segundo número:");
+        numero2 = reader.nextDouble();
+
+    }
+}
