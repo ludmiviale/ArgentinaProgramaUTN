9cc335b61fd5f6111cb91b3f9ab416a58389f0d0
ludmiviale <111137199+ludmiviale@users.noreply.github.com>
2023-04-14 11:17:26 -0300

    Actualizando TP8, excepciones y colecciones

--- TP5/src/Paquete1/ComprasConLista.java
+++ TP5/src/Paquete1/ComprasConLista.java
@@ -1,5 +1,6 @@
 package Paquete1;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -10,9 +11,9 @@ public class ComprasConLista {
         Producto producto1 = new Producto(0046, 220.00, "Agua mineral");
         Producto producto2 = new Producto(0234, 700.00, "Cerveza");
         Producto producto3 = new Producto(0341, 350.00, "Gaseosa");
-        Producto producto4 = new Producto(0120, 900.00, "Vino Malbec");
-        Producto producto5 = new Producto(0121, 850.00, "Vino Syrah");
-        Producto producto6 = new Producto(0122, 950.00, "Vino Cabernet");
+        Producto producto4 = new Producto(0120, 0.00, "Vino Malbec");
+        Producto producto5 = new Producto(0121, 0.00, "Vino Syrah");
+        Producto producto6 = new Producto(0122, 0.00, "Vino Cabernet");
 
         Cliente cliente1 = new Cliente("Juan Pérez", 33499851, "3425444377", "juanperez@gmail.com");
         Cliente cliente2 = new Cliente("Pedro Díaz", 39855712, "342137885", "pedrod@gmail.com");
@@ -56,45 +57,49 @@ public class ComprasConLista {
         itemLista3.add(item8);
         itemLista3.add(item9);
 
-        mostrarCompra(itemLista1, carrito1, cliente1, "f");
-        mostrarCompra(itemLista2, carrito2, cliente2, "%");
-        mostrarCompra(itemLista3, carrito3, cliente3, "%t");
+        mostrarCompra(itemLista1, carrito1, cliente1);
+        mostrarCompra(itemLista2, carrito2, cliente2);
+        mostrarCompra(itemLista3, carrito3, cliente3);
 
     }
 
-    public static void mostrarCompra(List<Item> itemLista, Carrito carrito, Cliente cliente, String tipoDesc) {
+    public static void mostrarCompra(List<Item> itemLista, Carrito carrito, Cliente cliente) {
 
         System.out.println("Carrito: " + carrito.getNum() + " - Cliente: " + cliente.getNombres());
         Iterator<Item> iteratorC = itemLista.iterator();
+
         while (iteratorC.hasNext()) {
             Item item = iteratorC.next();
             item.mostrarItem();
             carrito.setMontoTotal(item);
         }
+
         System.out.println("Total: " + carrito.getMontoTotal());
 
-        switch (tipoDesc) {
-            case "f": {
-                DescuentoFijo dFijo = new DescuentoFijo();
+        if (carrito.getMontoTotal() > 0) {
+
+            LocalDate diaDesc = LocalDate.now();
+            int descuento = diaDesc.getDayOfMonth();
+
+            if (descuento / 2 == 0) {
+                Descuento dFijo = new DescuentoFijo();
                 dFijo.setMontoDesc(300);
-                System.out.println("Monto final con descuento fijo: " +
-                        dFijo.montoFinal(carrito.getMontoTotal()));
-            }
-                break;
-            case "%": {
-                DescuentoPorcentaje dPorc = new DescuentoPorcentaje();
+                double montoDF = dFijo.getMontoDesc();
+                if (montoDF > 0)
+                    System.out.println("Monto final con descuento fijo: " + montoDF);
+                else
+                    System.out.println("El descuento no se puede aplicar a este carrito");
+            } else {
+                Descuento dPorc = new DescuentoPorcentaje();
                 dPorc.setMontoPorc(30);
-                System.out.println(
-                        "Monto final con descuento con porcentaje: " + dPorc.montoFinal(carrito.getMontoTotal()));
+                double montoDP = dPorc.montoFinal(carrito.getMontoTotal());
+                if (montoDP > 0)
+                    System.out.println("Monto final con descuento con porcentaje: " + montoDP);
+                else
+                    System.out.println("El descuento no se puede aplicar a este carrito");
             }
-                break;
-            case "%t": {
-                DescuentoPorcentajeConTope dPorcTope = new DescuentoPorcentajeConTope(2000);
-                dPorcTope.setMontoDesc(40);
-                System.out.println(
-                        "Monto final con descuento con porcentaje con tope: "
-                                + dPorcTope.montoFinal(carrito.getMontoTotal()));
-            }
+        } else {
+            System.out.println("No se puede aplicar descuentos en montos iguales a 0");
         }
     }
 }
